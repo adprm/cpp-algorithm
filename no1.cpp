d374edb3a6741152ee0e12bf1aa95843880cce3d
aditiya permana <aditiyaprmn00@gmail.com>
2019-11-27 15:09:00 +0700

    App Calculate the volume of water

---
+++ no1.cpp
@@ -0,0 +1,21 @@
+//SOAL NOMOR 1
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+    int t, l, p, v;
+    cout<<"        Menghitung Volume Air"<<endl;
+    cout<<"======================================"<<endl;
+    cout << "         Masukkan tinggi : ";
+    cin >> t;
+    cout << "          Masukkan lebar : ";
+    cin >> l;
+    cout << "          Masukkan lebar : ";
+    cin >> p;
+
+    v = t * l * p;
+    cout<<"======================================"<<endl;
+    cout << "           Volume air : " << v;
+}
