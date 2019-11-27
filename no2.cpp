ab487fa85b6a6bc0103b23b4062716918805b748
aditiya permana <aditiyaprmn00@gmail.com>
2019-11-27 15:09:41 +0700

    App Triangle type

---
+++ no2.cpp
@@ -0,0 +1,28 @@
+//SOAL NOMOR 2
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cout << "Menentukan jenis sebuah segitiga" << endl;
+    cout << "Masukkan sisi A : ";
+    cin >> a;
+    cout << "Masukkan sisi B : ";
+    cin >> b;
+    cout << "Masukkan sisi C : ";
+    cin >> c;
+
+    if (a == b && b == c)
+    {
+        cout << "Segitiga sama sisi";
+    }
+    else if (a == b || a == c || b == c)
+    {
+        cout << "Segitiga sama kaki";
+    }
+    else
+    {
+        cout << "Segitita sembarang";
+    }
+}
