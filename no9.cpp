02ccf9ee3986b3150eda49b54a45dc9ed594e442
aditiya permana <aditiyaprmn00@gmail.com>
2019-11-27 15:21:26 +0700

    App Every even number is marked

---
+++ no9.cpp
@@ -0,0 +1,19 @@
+//SOAL NOMOR 10
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    for (i = 1; i <= 100; i++)
+    {
+        if (i % 2 == 0)
+        {
+            cout << i << " genap" << endl;
+        }
+        else if (i % 2 == 1)
+        {
+            cout << i << endl;
+        }
+    }
+}
