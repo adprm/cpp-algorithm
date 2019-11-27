792b400919d4a0424b668df113c70cf626182194
aditiya permana <aditiyaprmn00@gmail.com>
2019-11-27 15:10:45 +0700

    App calculate the circumference of a circle

---
+++ no3.cpp
@@ -0,0 +1,15 @@
+//SOAL NOMOR 3
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float d, k;
+    cout << "Menghitung keliling sebuah lingkaran" << endl;
+    cout << "Masukkan diameter lingkaran : ";
+    cin >> d;
+
+    k = 3.14 * d;
+
+    cout << "Keliling lingkaran : " << k;
+}
