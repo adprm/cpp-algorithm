8e9f1b325a155de68c1412d79e4d89176db70664
aditiyapermana <aditiyaprmn00@gmail.com>
2020-01-24 16:30:21 +0700

    App loop retangle

---
+++ no10.cpp
@@ -0,0 +1,22 @@
+/* SOAL NOMOR 10
+author : Aditiya Permana 15190007 (peesegi) */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i, j, n;
+
+    cout << "Masukkan nilai = ";
+    cin >> n;
+
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= n; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
