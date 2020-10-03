86b753f96a817c1875dc44dfb1152b21bf749fbc
adprm <aditiyaprmn00@gmail.com>
2020-10-03 10:03:53 +0700

    program birthday

---
+++ birthday.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int umur;
+
+    cout<<"Masukkan umur teman anda : " ; cin >> umur;
+    cout<<"Selamat ulang tahun yang ke " << umur << " yaaaa!";
+
+    return 0;
+
+}
